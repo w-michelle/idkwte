f46a7b550773cabca268ea62e601cfe5bdc23e8a
w-michelle <89858197+w-michelle@users.noreply.github.com>
2023-07-21 14:08:05 -0400

    update api url

--- client/src/pages/Restaurant.jsx
+++ client/src/pages/Restaurant.jsx
@@ -13,7 +13,7 @@ function Restaurant() {
 
     const fetchDetails = async() => {
         try{
-            const response = await fetch(`/business?id=${params.id}`)
+            const response = await fetch(`https://idwte-api.vercel.app/business?id=${params.id}`)
             const data = await response.json()
             setDetails(data)
     
