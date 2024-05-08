87289ec5afe2b28aa7216bc6dfcc71c590f27cfb
w-michelle <89858197+w-michelle@users.noreply.github.com>
2024-05-08 16:14:39 -0400

    layout change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
   return (
     <motion.div animate={{opacity:1}} initial={{opacity:0}} exit={{opacity:0}} transition={{duration:0.5}}>      
           <Popular />
-          <Veggie />  
+     
     </motion.div>
   )
 }
